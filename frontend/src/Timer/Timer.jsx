9a3811f3025dc128a48d9041a230554a48ddcd0e
JaafarHawli <jaafarhawle@gmail.com>
2022-12-25 03:31:40 +0200

    added scale motion to the timer

--- frontend/src/Timer/Timer.jsx
+++ frontend/src/Timer/Timer.jsx
@@ -1,27 +1,41 @@
 import React, {useState, useEffect} from 'react';
 import './styles.css';
+import {motion, useAnimation } from 'framer-motion';
 
 const Timer = () => {
+
+  const timerAnimation = useAnimation();
 
   const [timer, setTimer] = useState(10);
   const [timeOut, setTimeOut] = useState(false);
 
   useEffect(() => {
+    timerAnimation.start({
+      scale: [1, 0],
+      transition: { duration: 1 },
+    });
+
     if(timer>=0)
     setTimeout(() => {
-      setTimer(timer-1)
+      setTimer(timer-1);
+      timerAnimation.start({
+        scale: [1, 0],
+        transition: { duration: 1 },
+      });
     }, 1000);
+
     else
     setTimeOut(true);
-  }, [timer]);
+  }, [timer, timerAnimation]);
 
+  
   return (
     <div className='container'>
       {
         timeOut ?
-        <h1 className='headline'>Happy New Year</h1>
+        <motion.h1 className='headline'>Happy New Year</motion.h1>
         :
-        <div className='timer'>{timer}</div>    
+        <motion.div className='timer' animate={timerAnimation}>{timer}</motion.div>    
       }
     </div>
   );
