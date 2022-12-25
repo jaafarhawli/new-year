de63a3d5475cf6ebfa110c80614656e44bdce7f4
JaafarHawli <jaafarhawle@gmail.com>
2022-12-25 05:57:16 +0200

    finalised and added comments

--- frontend/src/Timer/Timer.jsx
+++ frontend/src/Timer/Timer.jsx
@@ -1,6 +1,6 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import './styles.css';
-import {motion, useAnimation, AnimatePresence } from 'framer-motion';
+import {motion, useAnimation } from 'framer-motion';
 import { CircularProgressbarWithChildren } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 
@@ -12,6 +12,7 @@ const Timer = () => {
   const [timeOut, setTimeOut] = useState(false);
 
   useEffect(() => {
+    // Animate the timer when the page renders for the first time
     timerAnimation.start({
       scale: [1, 0.5],
       opacity: [1, 0],
@@ -19,7 +20,8 @@ const Timer = () => {
     });
 
     setTimeout(() => {
-      if(timer!==0) {
+      if(timer!==0) { 
+      // While the timer hasn't reached zero, decrement it by 1 every second and repeat the animation
       setTimer(timer-1);
       timerAnimation.start({
         scale: [1, 0.5],
@@ -34,27 +36,37 @@ const Timer = () => {
 
 
   return (
-    <AnimatePresence>
-      <motion.div className={timeOut ? 'container-timeout' : 'container'}>
+      <div className={ timeOut ? 'container-timeout' : 'container' }>
         {
+          // If the timer is over, display the happy new year text with letter by letter animation
           timeOut ?
           <motion.h1 className='headline'>{"Happy New Year".split('').map((letter, index) => (
-            <motion.span key={index} initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: index * 0.1 }}
-            variants={{
-              hidden: { opacity: 0 },
-              visible: { opacity: 1 },
-            }}>
-              {letter}
+            <motion.span 
+              key={ index } 
+              initial={{ opacity: 0 }} 
+              animate={{ opacity: 1 }} 
+              transition={{ delay: index * 0.1 }}
+              >
+              { letter }
             </motion.span>
           ))}</motion.h1>
+
           :
-          <CircularProgressbarWithChildren value={100-timer*10} styles={{path:{
-            stroke: '#C91E1B'}}}>
-            <motion.div className='timer' animate={timerAnimation}>{timer}</motion.div> 
+          // If the timer is still counting, display it with a circular progress bar around it
+          <CircularProgressbarWithChildren 
+            value={ 100-timer*10 } 
+            styles={{
+              path:{
+              stroke: '#C91E1B'
+          }}}>
+            <motion.div 
+              className='timer' 
+              animate={ timerAnimation }>
+              { timer }  
+            </motion.div> 
           </CircularProgressbarWithChildren>         
         }
-      </motion.div>
-    </AnimatePresence>
+      </div>
   );
 }
 
