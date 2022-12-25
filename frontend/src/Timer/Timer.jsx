14a7fbe501f8cef348705cf0df487004cbb5a823
JaafarHawli <jaafarhawle@gmail.com>
2022-12-25 05:30:25 +0200

    added gif background

--- frontend/src/Timer/Timer.jsx
+++ frontend/src/Timer/Timer.jsx
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import './styles.css';
-import {motion, useAnimation } from 'framer-motion';
+import {motion, useAnimation, AnimatePresence } from 'framer-motion';
 import { CircularProgressbarWithChildren } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 
@@ -34,17 +34,27 @@ const Timer = () => {
 
 
   return (
-    <div className='container'>
-      {
-        timeOut ?
-        <motion.h1 className='headline'>Happy New Year</motion.h1>
-        :
-        <CircularProgressbarWithChildren value={100-timer*10} styles={{path:{
-          stroke: '#C91E1B'}}}>
-          <motion.div className='timer' animate={timerAnimation}>{timer}</motion.div> 
-        </CircularProgressbarWithChildren>         
-      }
-    </div>
+    <AnimatePresence>
+      <motion.div className={timeOut ? 'container-timeout' : 'container'}>
+        {
+          timeOut ?
+          <motion.h1 className='headline'>{"Happy New Year".split('').map((letter, index) => (
+            <motion.span key={index} initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: index * 0.1 }}
+            variants={{
+              hidden: { opacity: 0 },
+              visible: { opacity: 1 },
+            }}>
+              {letter}
+            </motion.span>
+          ))}</motion.h1>
+          :
+          <CircularProgressbarWithChildren value={100-timer*10} styles={{path:{
+            stroke: '#C91E1B'}}}>
+            <motion.div className='timer' animate={timerAnimation}>{timer}</motion.div> 
+          </CircularProgressbarWithChildren>         
+        }
+      </motion.div>
+    </AnimatePresence>
   );
 }
 
